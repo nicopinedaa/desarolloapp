f7b5a0ef37ada588b61f34650d3445ea396fbaa6
nicopinedaa <nicolepineda1995@gmail.com>
2022-08-25 19:46:48 -0500

    Primer trabajo

---
+++ app/src/main/java/com/anahuac/anahuac/MainActivity.java
@@ -0,0 +1,33 @@
+package com.anahuac.anahuac;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnLoginFacebook;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnLoginFacebook = findViewById(R.id.btn_login_facebook);
+
+        btnLoginFacebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Boton pulsado" , Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
+
